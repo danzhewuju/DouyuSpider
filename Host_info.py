d0fdc9aa3b85304f72d76b2f2a02212ed1073458
单者无惧 <1668926294@qq.com>
2018-07-24 11:57:38 +0800

    爬取斗鱼直播相关信息

---
+++ Host_info.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from typing import Any
+
+
+class Host:
+    id = 0
+    username = ""
+    kind = ""
+    w_number = 0
+    followers = 0
+    coefficient = 0.0
+    localtime = ""
+
+
+
---
+++ UnitMysql.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import pymysql
+from Host_info import *
+import time,datetime
+
+
+class Unit_Mysql:
+    def connect_db(self):
+        return pymysql.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='root',
+            database='douyu',
+            charset='utf8'
+        )
+
+    def insert_db(self, host):
+        con = self.connect_db()
+        cur = con.cursor()
+        try:
+            sql_str = "insert into host_info  (username, kind, timestamp, w_number, followers, coefficient)" \
+                      " values ('%s', '%s','%s','%s','%s','%s')" \
+                      % (host.username, host.kind, host.localtime, host.w_number, host.followers, host.coefficient)
+            cur.execute(sql_str)
+            con.commit()
+        except:
+            con.rollback()
+            print("插入错误!")
+            raise
+        finally:
+            cur.close()
+            con.close()
+
+
+# host = Host()
+# host.username = "大仙"
+# host.kind = "王者荣耀"
+# time1 = int(time.time())
+# print(time1)
+# timearray = time.localtime(time1)
+# datetime1 = time.strftime("%Y-%m-%d %H:%M:%S")
+# host.localtime = str(datetime1)
+# print(host.localtime)
+# host.w_number = 1960000
+# host.followers = 12000000
+# host.coefficient = host.w_number / host.followers
+# user =Unit_Mysql()
+# user.insert_db(host)
