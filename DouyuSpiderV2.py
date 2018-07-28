dccb29260d78ea370df0b0cad16696fe3a4b74ed
单者无惧 <1668926294@qq.com>
2018-07-29 06:16:05 +0800

    爬取斗鱼直播相关信息,完善了获取全部类别,获取全部直播间信息！

---
+++ DouyuSpiderV2.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+import re
+import requests
+from Host_info import *
+from UnitMysql import *
+import time
+import json
+
+
+def open_html(url):
+    r = requests.get(url)
+    r.encoding = 'utf-8'
+    return r.text
+
+
+def get_online_number(room_number):       #获取斗鱼的实际在线人数
+    url = "https://www.douyu.com/swf_api/h5room/" + room_number
+    html = open_html(url)
+    try:
+        json_a = json.loads(html)
+        online_number = 0
+        online_number = int(json_a['data']['online'])
+    except:
+        online_number = 0
+    finally:
+        return online_number
+
+
+def caculate_rate(w_watching, online):  #计算影响因子
+    result = 0.0
+    if online == 0:
+        return result
+    else:
+        result = w_watching / online
+    return result
+
+
+def get_info():
+    a = 1   #每一个类型的直播间 比如：王者荣耀
+    b = 1   #每个直播间的分类
+    pages= 0
+    count = 0
+    save_a = 0
+    sum = 600 #设置最多的直播间数量
+    while a < sum :
+        b = 1
+        url = "https://www.douyu.com/gapi/rkc/directory/2_"+str(a)+"/"+str(b)
+        html = open_html(url)
+        counta = 1
+        countb = 1
+        rid =""
+        username =""
+        w_watching =""
+        kind = ""
+        online = 0
+
+        rid = ""
+        json_a = json.loads(html)
+        pages = json_a['data']['pgcnt']
+        while b <= pages:
+            url = "https://www.douyu.com/gapi/rkc/directory/2_" + str(a) + "/" + str(b)  # 更新链接
+            html = open_html(url)
+            json_a = json.loads(html)
+            for data in json_a['data']['rl']:
+                rid = str(data['rid'])
+                username = data['nn']
+                w_watching = data['ol']
+                kind = data['c2name']
+                online = get_online_number(rid)
+                coefficient = caculate_rate(w_watching, online)
+                link = "https://www.douyu.con/" + rid
+                datetime1 = time.strftime("%Y-%m-%d %H:%M:%S")
+                host = Host()
+
+                host.username = username
+                host.online = online
+                host.w_number = w_watching
+                host.kind = kind
+                host.room_number = rid
+                host.coefficient = coefficient
+                host.link = link
+                host.localtime =datetime1
+                unit = Unit_Mysql()
+                unit.insert_db(host)
+                count += 1
+                print("正在抓取第%d条数据，%s（%s:第%d/%d页）" % (count, username, kind, b, pages))
+            b += 1
+        b = 1
+        if pages != 0:
+            save_a = a
+        a += 1
+    print(save_a)
+
+
+get_info()
--- test.py
+++ test.py
@@ -1,3 +1,52 @@
 #!/usr/bin/python3
 import json
-from DouyuSpider import *
+from bs4 import BeautifulSoup
+import re
+import requests
+import time
+
+
+def open_html(url):
+    r = requests.get(url)
+    r.encoding = 'utf-8'
+    return r.text
+
+
+def get_page(url):
+    html = open_html(url)
+    json1 = json.loads(html)
+    print(json1)
+
+
+def get_info():
+    a = 1   #每一个类型的直播间 比如：王者荣耀
+    b = 1   #每个直播间的分类
+    pages= 0
+    sum = 600 #设置最多的直播间数量
+    while a < sum :
+        b = 1
+        url = "https://www.douyu.com/gapi/rkc/directory/2_"+str(a)+"/"+str(b)
+        html = open_html(url)
+        rid =""
+        username =""
+        w_watching =""
+        kind = ""
+        json_a = json.loads(html)
+        pages = json_a['data']['pgcnt']
+        while b <= pages:
+            url = "https://www.douyu.com/gapi/rkc/directory/2_" + str(a) + "/" + str(b)  # 更新链接
+            html = open_html(url)
+            json_a = json.loads(html)
+            for data in json_a['data']['rl']:
+                rid = data['rid']
+                username = data['nn']
+                w_watching = data['ol']
+                kind = data['c2name']
+                print(rid, "\t", username, "\t", w_watching, "\t", kind)
+            print(b, pages)
+            b += 1
+        b = 1
+        a += 1
+
+
+get_info()
